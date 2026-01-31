7f0bc0d6f5fa82aacb3f00622bf8e7a94364dbc9
SANTANU NANDI <santanu2005nandi@gmail.com>
2026-01-31 19:09:03 +0530

    Fix import case for Button component

--- src/pages/SingleProduct.jsx
+++ src/pages/SingleProduct.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useParams, Link, useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import axios from "../axiosWithJwt";
-import { Button } from "@/components/ui/button";
+import { Button } from "@/components/ui/Button";
 import { toast } from "sonner";
 import { setCart } from "@/redux/productSlice";
 
